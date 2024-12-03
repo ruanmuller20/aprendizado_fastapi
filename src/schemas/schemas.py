f7dd4bd4f5d85e97b0022e9ceb6c1c66349bf515
ruanmuller20 <ruanmuller20@gmail.com>
2024-12-02 23:15:53 -0300

    Criação da estrutura do projeto

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -6,22 +6,21 @@ class User(BaseModel):
     id: Optional[str] = None
     nome: str
     telefone: str
-    meus_produtos: List['Produto']
-    minhas_vendas: List['Pedido']
-    meus_pedidos: List['Pedido']
+
     
 class Produto(BaseModel):
     id: Optional[str] = None
-    usuario: User
     nome: str
     detalhes: str
     preco: float
     disponivel: bool = False
     
+    class Config:
+        orm_mode = True
+    
+    
 class Pedido(BaseModel):
     id: Optional[str] = None
-    usuario: User
-    produto: Produto
     quantidade: int
     entrega: bool = True
     endereco: str
