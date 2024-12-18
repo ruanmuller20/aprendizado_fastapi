025918eb83fe202f86831a3e5780910479fe6344
ruanmuller20 <ruanmuller20@gmail.com>
2024-12-17 21:28:50 -0300

    Migrations BD

--- src/infra/sqlalchemy/repositorios/produto.py
+++ src/infra/sqlalchemy/repositorios/produto.py
@@ -2,28 +2,30 @@ from sqlalchemy.orm import Session
 from src.schemas import schemas
 from src.infra.sqlalchemy.models import models
 
-
-class RepositorioProduto():
-    
+class RepositorioProduto:
     def __init__(self, db: Session):
         self.db = db
-    
+
     def criar(self, produto: schemas.Produto):
-        db_produto = models.Produto(nome=produto.nome, 
-                                    detalhes=produto.detalhes,
-                                    preco=produto.preco,
-                                    disponivel=produto.disponivel)
+        db_produto = models.Produto(
+            nome=produto.nome, 
+            detalhes=produto.detalhes,
+            preco=produto.preco,
+            disponivel=produto.disponivel
+        )
         self.db.add(db_produto)
         self.db.commit()
         self.db.refresh(db_produto)
-        return db_produto
-    
+        return schemas.ProdutoSimples(
+            id=str(db_produto.id),  # Convertendo id para string
+            nome=db_produto.nome,
+            preco=db_produto.preco
+        )
+
     def listar(self):
         produtos = self.db.query(models.Produto).all()
-        return produtos
-    
-    def obter(self):
-        pass
-    
-    def remover(self):
-        pass        
+        return [schemas.ProdutoSimples(
+            id=str(produto.id),  # Convertendo id para string
+            nome=produto.nome,
+            preco=produto.preco
+        ) for produto in produtos]
